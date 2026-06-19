4aa6790ba70d83f78af95e8549c701bc9625b55b
TheLitis <lovalentin08@gmail.com>
2026-06-19 14:59:18 +0000

    feat(sources): register all sources in the package

--- kairos_text/sources/__init__.py
+++ kairos_text/sources/__init__.py
@@ -1,3 +1,13 @@
+from .base import EventSource
+from .brightdata_reddit import BrightDataRedditSource
+from .brightdata_x import BrightDataXSource
+from .gdelt import GDELTSource
 from .rss import RSSSource
 
-__all__ = ["RSSSource"]
+__all__ = [
+    "EventSource",
+    "GDELTSource",
+    "RSSSource",
+    "BrightDataXSource",
+    "BrightDataRedditSource",
+]
